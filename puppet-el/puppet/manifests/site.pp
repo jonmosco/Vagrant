2e0483157e984990fd50428ee528631fe952620c
Jon Mosco <jonny.mosco@gmail.com>
2013-06-18 11:37:34 -0400

    update firewall configuration

--- puppet-el/puppet/manifests/site.pp
+++ puppet-el/puppet/manifests/site.pp
@@ -13,16 +13,16 @@ node 'vtest1.forkedprocess.com' {
   # Install Apache
   #class {'apache': }
 
-  resources { 'firewall':
-      purge => true
-  }
-
-  Firewall {
-      before    => Class['my_fw::post'],
-        require => Class['my_fw::pre'],
-  }
-
-  class { ['my_fw::pre', 'my_fw::post']: }
+  #  resources { 'firewall':
+  #      purge => true
+  #  }
+  #
+  #  Firewall {
+  #      before    => Class['my_fw::post'],
+  #        require => Class['my_fw::pre'],
+  #  }
+  #
+  #  class { ['my_fw::pre', 'my_fw::post']: }
 
   class { 'firewall': }
 
