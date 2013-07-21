e68b927c048d29b21ec5bd852a1c7de412f3c2f1
Jon Mosco <jonny.mosco@gmail.com>
2013-07-21 10:35:01 -0400

    updated site manifest

--- capistrano/puppet/manifests/site.pp
+++ capistrano/puppet/manifests/site.pp
@@ -1,2 +1,25 @@
+#
+# Setup our source and destination server for Capistrano
+# testing
+#
 node 'source.forkedprocess.com' {
+  exec { 'apt-get update':
+    path => '/usr/bin',
+  }
+
+}
+
+node 'dest.forkedprocess.com' {
+
+  $packages = [ 'apache2', 'subversion' ]
+
+  exec { 'apt-get update':
+    path => '/usr/bin',
+  }
+
+  package { $packages:
+    ensure  => installed,
+    require => Exec['apt-get update'],
+  }
+
 }
