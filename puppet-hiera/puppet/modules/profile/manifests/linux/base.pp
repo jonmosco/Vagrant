bb85fab60cd4b04b2338204c8650ddf31efde0ec
Jon Mosco <jonny.mosco@gmail.com>
2013-08-03 20:59:28 -0400

    added modules for more practice

--- puppet-hiera/puppet/modules/profile/manifests/linux/base.pp
+++ puppet-hiera/puppet/modules/profile/manifests/linux/base.pp
@@ -7,4 +7,11 @@ class profile::linux::base {
 
   # selinux config
   include puppet_selinux
+
+  firewall { '100 allow http and https access':
+    port   => [80, 443],
+    proto  => tcp,
+    action => accept,
+  }
+
 }
